f17babaf61676e463daeec0bcc6689bc12c4e6ff
peryaudo <peryaudo@gmail.com>
2015-03-24 19:27:02 +0900

    Fixed

--- www/audizz-recorder.js
+++ www/audizz-recorder.js
@@ -1,18 +1,21 @@
+var utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+
 function AudizzRecorder(src) {
   this.id = utils.createUUID();
   this.src = src;
 }
 
 AudizzRecorder.prototype.start = function start() {
-  cordova.exec(null, null, 'AudizzRecorderPlugin', 'start', [this.id, this.src]);
+  exec(null, null, 'AudizzRecorderPlugin', 'start', [this.id, this.src]);
 };
 
 AudizzRecorder.prototype.stop = function stop() {
-  cordova.exec(null, null, 'AudizzRecorderPlugin', 'stop', [this.id]);
+  exec(null, null, 'AudizzRecorderPlugin', 'stop', [this.id]);
 };
 
 AudizzRecorder.prototype.getLevel = function getLevel(success) {
-  cordova.exec(success, null, 'AudizzRecorderPlugin', 'getLevel', [this.id]);
+  exec(success, null, 'AudizzRecorderPlugin', 'getLevel', [this.id]);
 };
 
 module.exports = AudizzRecorder;
